44ed9ec948e8af69aac2c12dfc63f07a2e8c8b8f
Andrew Tugarinov <nalcheg@gmail.com>
2020-01-20 23:34:05 +0700

    Added fasthttp client POST request example.

--- http/fasthttp-client/main.go
+++ http/fasthttp-client/main.go
@@ -18,3 +18,25 @@ func DoRequest(url string) (string, error) {
 
 	return string(bodyBytes), nil
 }
+
+func DoPostRequestExample(url string) (string, error) {
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseRequest(req)
+	defer fasthttp.ReleaseResponse(resp)
+
+	req.SetRequestURI(url)
+	req.Header.SetMethodBytes([]byte("POST"))
+	req.PostArgs().Add("value", "0.13")
+
+	client := fasthttp.Client{}
+	client.MaxConnsPerHost = 1000
+
+	if err := client.Do(req, resp); err != nil {
+		return "", nil
+	}
+
+	bodyBytes := resp.Body()
+
+	return string(bodyBytes), nil
+}
