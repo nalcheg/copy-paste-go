867bd9bf0f7c87a301ff53bccb74a2803d58e640
Andrew Tugarinov <nalcheg@gmail.com>
2020-01-21 22:07:41 +0700

    Refactored errgroups tests.

--- concurrency/error-groups/main_test.go
+++ concurrency/error-groups/main_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"log"
 	"testing"
+	"time"
 
 	"golang.org/x/sync/errgroup"
 )
@@ -10,18 +12,18 @@ import (
 func TestPositive(t *testing.T) {
 	app := NewApp("first", "second", "third")
 
-	gr := errgroup.Group{}
+	errGr := errgroup.Group{}
 
-	gr.Go(func() error {
+	errGr.Go(func() error {
 		_, err := app.GetFirst()
 		return err
 	})
-	gr.Go(func() error {
+	errGr.Go(func() error {
 		_, err := app.GetThird()
 		return err
 	})
 
-	if err := gr.Wait(); err != nil {
+	if err := errGr.Wait(); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -29,22 +31,74 @@ func TestPositive(t *testing.T) {
 func TestError(t *testing.T) {
 	app := NewApp("first", "second", "third")
 
-	gr := errgroup.Group{}
+	errGr := errgroup.Group{}
 
-	gr.Go(func() error {
+	errGr.Go(func() error {
 		_, err := app.GetFirst()
 		return err
 	})
-	gr.Go(func() error {
+	errGr.Go(func() error {
 		_, err := app.GetSecond()
 		return err
 	})
-	gr.Go(func() error {
+	errGr.Go(func() error {
 		_, err := app.GetThird()
 		return err
 	})
 
-	if err := gr.Wait(); err != errorSecond {
+	if err := errGr.Wait(); err != errorSecond {
 		log.Fatal(err)
+	}
+}
+
+func TestErrgroupWithContext(t *testing.T) {
+	tests := []struct {
+		name        string
+		timeout     time.Duration
+		wantedError error
+	}{
+		{
+			name:        "big timeout",
+			timeout:     1000 * time.Millisecond,
+			wantedError: context.Canceled,
+		}, {
+			name:        "too small timeout",
+			timeout:     10 * time.Millisecond,
+			wantedError: context.DeadlineExceeded,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			app := NewApp("first", "second", "third")
+			ctx, cancel := context.WithTimeout(context.Background(), test.timeout)
+			defer cancel()
+
+			errGr, ctx := errgroup.WithContext(ctx)
+
+			go func() {
+				select {
+				case <-ctx.Done():
+					// run something on context timeout
+					log.Print("timeout error ", time.Now())
+				}
+			}()
+
+			errGr.Go(func() error {
+				_, err := app.GetFirst()
+				return err
+			})
+			errGr.Go(func() error {
+				_, err := app.GetThird()
+				return err
+			})
+
+			if err := errGr.Wait(); err != nil {
+				t.Error(err)
+			}
+
+			if test.wantedError != ctx.Err() {
+				t.Error("unexpected context error")
+			}
+		})
 	}
 }
