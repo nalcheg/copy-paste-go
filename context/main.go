541145ef1bc5493764614777c15c8cad676225ee
Andrew Tugarinov <nalcheg@gmail.com>
2020-01-27 14:55:29 +0700

    Added http handler with context example.

--- context/main.go
+++ context/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 	"time"
 
+	"github.com/brianvoe/gofakeit"
 	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
 )
@@ -20,9 +25,12 @@ type App struct {
 }
 
 func (a *App) Start(addr string) {
+	gofakeit.Seed(0)
+
 	r := router.New()
 	r.GET("/", now)
 	r.GET("/sleep", sleep)
+	r.GET("/beer", fastHTTPHandlerWithTimeout)
 
 	go func() {
 		log.Fatal(fasthttp.ListenAndServe(addr, r.Handler))
@@ -45,4 +53,48 @@ func now(ctx *fasthttp.RequestCtx) {
 func sleep(ctx *fasthttp.RequestCtx) {
 	time.Sleep(5 * time.Second)
 	ctx.SetStatusCode(204)
+}
+
+type ResultData struct {
+	Data string `json:"data"`
+}
+
+func fastHTTPHandlerWithTimeout(ctx *fasthttp.RequestCtx) {
+	c, cancel := context.WithTimeout(ctx, 100*time.Millisecond)
+	defer cancel()
+
+	resp := make(chan ResultData)
+
+	go process(c, resp)
+
+	var res ResultData
+	select {
+	case <-c.Done():
+		fmt.Printf("error: processing timeout \n")
+		break
+	case res = <-resp:
+		fmt.Printf("finished processing\n")
+	}
+
+	if res.Data == "" {
+		ctx.SetStatusCode(http.StatusRequestTimeout)
+		return
+	}
+
+	r, err := json.Marshal(res)
+	if err != nil {
+		panic(err)
+	}
+	if _, err := ctx.Write(r); err != nil {
+		panic(err)
+	}
+	ctx.SetStatusCode(http.StatusOK)
+}
+
+func process(ctx context.Context, respChan chan<- ResultData) {
+	randomTimeout := gofakeit.Number(50, 150)
+
+	time.Sleep(time.Duration(randomTimeout) * time.Millisecond)
+
+	respChan <- ResultData{Data: gofakeit.BeerName()}
 }
